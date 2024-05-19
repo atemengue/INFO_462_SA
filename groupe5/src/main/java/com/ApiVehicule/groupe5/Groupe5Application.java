2c46f68318a779be59a50159683371a6ff5c58f9
Moussaabakar4 <moussaabakar4@gmail.com>
2024-05-19 18:42:33 +0100

    Add files via upload

---
+++ groupe5/src/main/java/com/ApiVehicule/groupe5/Groupe5Application.java
@@ -0,0 +1,13 @@
+package com.ApiVehicule.groupe5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupe5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Groupe5Application.class, args);
+	}
+
+}
