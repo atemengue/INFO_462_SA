d267074c81d09fb9791c5c17373f30ea2c5dedeb
jordantsagmo <jordantsgamo@gmail.com>
2024-05-27 17:07:35 +0200

    initialisation du projet

---
+++ properlize_App/src/main/java/com/properlize/properlize_app/ProperlizeAppApplication.java
@@ -0,0 +1,14 @@
+package com.properlize.properlize_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProperlizeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProperlizeAppApplication.class, args);
+    }
+
+}
+
---
+++ properlize_App/src/test/java/com/properlize/properlize_app/ProperlizeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.properlize.properlize_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProperlizeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
